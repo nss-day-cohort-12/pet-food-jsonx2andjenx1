9741f3efd46d6fea3ec1d81b7b61a5352ba0c13c
Jen Solima <jensolima@gmail.com>
2016-02-05 08:49:39 -0600

    cleaned up code and added comments

--- food.js
+++ food.js
@@ -1,95 +1,76 @@
-// console.log("First line in JavaScript file");
-// console.log(Date.now());
 
-function executeThisCodeAfterFileIsLoadedDog () {
-  // console.log("executeThisCodeAfterFileIsLoaded");
-  // console.log(Date.now())
-  // console.log("this.responseText", this.responseText);
-  var data = JSON.parse(this.responseText);
-  //console.log("data", data);
-  var contentDogDiv = document.getElementById("all-my-dog-food"); 
-  var dogFoodData = "";
+function executeThisCodeAfterFileIsLoadedDog () { // DOG
 
-  console.log("data", data);
+  //console.log("this.responseText", this.responseText);
+  var data = JSON.parse(this.responseText); // parse the json data to readable javascript
+  // console.log("data", data);
+  var contentDogDiv = document.getElementById("all-my-dog-food");  // find div with correct id
+  var dogFoodData = "";  // start with empty string
 
-  for (var i = 0; i < data.dog_brands.length; i++) {
-    currentFood = data.dog_brands[i];
-    dogFoodData += "<div class='food-block'>"; // open div with class of food-block 
+  // console.log("data.dog_brands", data.dog_brands);
+  for (var i = 0; i < data.dog_brands.length; i++) {  // loop thru DOG_BRANDS array
+    // console.log("data.dog_brands[i]", data.dog_brands[i]);
+    currentFood = data.dog_brands[i];  
+    dogFoodData += "<div class='food-block'>";  // open div with class of food-block 
     dogFoodData += `<h2>${currentFood.name}</h2>`;  // add brand NAME (Chuck Wagon)
 
-    for (var j = 0; j < currentFood.types.length; j++){
-      currentType = currentFood.types[j];
+    for (var j = 0; j < currentFood.types.length; j++){  // loop thru TYPES array
+      // console.log("currentFood.types[j]", currentFood.types[j]);
+      currentType = currentFood.types[j]; 
       dogFoodData += "<div class='type'>Type: " + currentType.type + "</div>"; //add food TYPE (All Natural)
 
-      for (var k = 0; k < currentType.volumes.length; k++){
-        currentVolume = currentType.volumes[k];
+      for (var k = 0; k < currentType.volumes.length; k++){  // loop thru VOLUMES array (name, price)
+        // console.log("currentType.volumes[k]", currentType.volumes[k]);
+        currentVolume = currentType.volumes[k]; 
         dogFoodData += "<div class='price'>Price: " + currentVolume.name + " for " + currentVolume.price + "</div>"; // add NAME and PRICE (32oz for 9.99)
       }
     } 
-    dogFoodData += "</div>"; // close div with class of food-block 
+    dogFoodData += "</div>";  // close div with class of food-block 
   }
-  contentDogDiv.innerHTML = dogFoodData; // add to page
-  console.log("dogFoodData", dogFoodData);
+  // console.log("dogFoodData", dogFoodData);
+  contentDogDiv.innerHTML = dogFoodData;  // add content to inner html
 }
 
-function executeThisCodeIfXHRFails (xhrEvent) {
-  console.log("An error occurred while transferring the data");
-}
-
-function executeThisCodeChunksArrive (xhrEvent) {
-
-}
-
-function executeThisCodeAfterFileIsLoadedCat () {
-  // console.log("executeThisCodeAfterFileIsLoaded");
-  // console.log(Date.now())
-  // console.log("this.responseText", this.responseText);
-  var data = JSON.parse(this.responseText);
+function executeThisCodeAfterFileIsLoadedCat () { // CAT
+  
+  //console.log("this.responseText", this.responseText);
+  var data = JSON.parse(this.responseText); // parse the json data to readable javascript
   //console.log("data", data);
-  var contentCatDiv = document.getElementById("all-my-cat-food"); 
-  var catFoodData = "";
+  var contentCatDiv = document.getElementById("all-my-cat-food");  // find div with correct id
+  var catFoodData = "";  // start with empty string
 
-  for (var i = 0; i < data.cat_brands.length; i++) {
-    currentFood = data.cat_brands[i];
-    catFoodData += "<div class='food-block'>"; // open div with class of food-block 
+  for (var i = 0; i < data.cat_brands.length; i++) {  // loop thru CAT_BRANDS array
+    // console.log("data.cat_brands[i]", data.cat_brands[i]);
+    currentFood = data.cat_brands[i]; 
+    catFoodData += "<div class='food-block'>";  // open div with class of food-block 
     catFoodData += `<h2>${currentFood.name}</h2>`;  // add brand NAME (Chuck Wagon)
 
-    for (var j = 0; j < currentFood.types.length; j++){
-      currentType = currentFood.types[j];
-      catFoodData += "<div class='type'>Type: " + currentType.type + "</div>"; //add food TYPE (All Natural)
+    for (var j = 0; j < currentFood.types.length; j++){  // loop thru TYPES array
+      // console.log("currentFood.types[j]", currentFood.types[j]);
+      currentType = currentFood.types[j]; 
+      catFoodData += "<div class='type'>Type: " + currentType.type + "</div>";  //add food TYPE (All Natural)
 
-      for (var k = 0; k < currentType.volumes.length; k++){
-        currentVolume = currentType.volumes[k];
+      for (var k = 0; k < currentType.volumes.length; k++){  // loop thru VOLUMES array (name, price)
+        // console.log("currentType.volumes[k]", currentType.volumes[k]);        
+        currentVolume = currentType.volumes[k]; 
         catFoodData += "<div class='price'>Price: " + currentVolume.name + " for " + currentVolume.price + "</div>"; // add NAME and PRICE (32oz for 9.99)
       }
     } 
-    catFoodData += "<div class='breeds'>Suggested for these breeds: " + currentFood.breeds;
     //console.log("console log breeds", currentFood.breeds);
-    catFoodData += "</div>"; // close div with class of food-block 
+    catFoodData += "<div class='breeds'>Suggested for these breeds: " + currentFood.breeds; // add cat BREEDS
+    catFoodData += "</div>";  // close div with class of food-block 
   }
-  contentCatDiv.innerHTML = catFoodData; // add to page
+  contentCatDiv.innerHTML = catFoodData; // add content to inner html
 }
 
-function executeThisCodeIfXHRFails (xhrEvent) {
-  console.log("An error occurred while transferring the data");
-}
+var myRequestDog = new XMLHttpRequest();  // dog XMLHttp Request
+var myRequestCat = new XMLHttpRequest();  // cat XMLHttp Request
 
-function executeThisCodeChunksArrive (xhrEvent) {
+myRequestDog.addEventListener("load", executeThisCodeAfterFileIsLoadedDog); // run dog function
+myRequestCat.addEventListener("load", executeThisCodeAfterFileIsLoadedCat); // run cat function
 
-}
+myRequestDog.open("GET", "dog.JSON"); // get & open the dog json file
+myRequestCat.open("GET", "cats.json"); // get & open the cat json file
 
-var myRequestDog = new XMLHttpRequest();
-var myRequestCat = new XMLHttpRequest();
-
-myRequestDog.addEventListener("load", executeThisCodeAfterFileIsLoadedDog);
-myRequestCat.addEventListener("load", executeThisCodeAfterFileIsLoadedCat);
-// myRequest.addEventListener("error", executeThisCodeIfXHRFails);
-// myRequest.addEventListener("progress", executeThisCodeChunksArrive);
-
-myRequestDog.open("GET", "dog.JSON");
-myRequestCat.open("GET", "cats.json");
-myRequestDog.send();
-myRequestCat.send();
-
-// console.log("Last line in JavaScript file");
-// console.log(Date.now());
+myRequestDog.send(); // execute function
+myRequestCat.send(); // execute function
